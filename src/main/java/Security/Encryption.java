2f433907dbe75d223ad2f50eb827f6b7a913ee8b
SolisCommet <nhatquangvl2003@gmail.com>
2022-10-11 10:31:11 +0700

    Methods for creating salt

--- src/main/java/Security/Encryption.java
+++ src/main/java/Security/Encryption.java
@@ -62,4 +62,59 @@ public class Encryption {
                 .decode(cipherText));
         return new String(plainText);
     }
+    
+    public static StringBuffer encryptCC(String text, int s)
+    {
+        StringBuffer result= new StringBuffer();
+ 
+        for (int i=0; i<text.length(); i++)
+        {
+            if (Character.isUpperCase(text.charAt(i)))
+            {
+                char ch = (char)(((int)text.charAt(i) +
+                                  s - 65) % 26 + 65);
+                result.append(ch);
+            }
+            else
+            {
+                char ch = (char)(((int)text.charAt(i) +
+                                  s - 97) % 26 + 97);
+                result.append(ch);
+            }
+        }
+        return result;
+    }
+    
+    public static String generateSpecChar(){
+        int max = 47;
+        int min = 33;
+        String res = "";
+        for (int i = 0;i<10;i++){
+            int in = ((int)Math.floor(Math.random()*(max-min+1)+min));
+            int num= ((int)Math.floor(Math.random()*(9+1)));
+            res+=num;
+            res+=(char)in;
+        }
+        min = ((int)Math.floor(Math.random()*(14+1)));
+        return res.substring(min, min+6);
+    }
+    
+    public static String generateSalt(String username, String password){
+        System.out.println("Username: " + username + " Password: " +password);
+        int max = username.length() + password.length();
+        int rand = (int)Math.floor(Math.random()*(max-1+1)+1);
+        if (max<10)
+            return "Unable to generate salt";
+        String salt = encryptCC(password+username, rand).toString();
+        salt = salt.substring(0, 6) + generateSpecChar() + salt.substring(6,11);
+        System.out.println("Salt: "+ salt);
+        return salt;
+    }
+    
+    public static void main(String[] args) {
+        generateSalt("QuangPNCE170036", "group4prj301");
+        generateSalt("QuangPNCE170036", "group4prj301");
+        generateSalt("QuangPNCE170036", "group4prj301");
+//        System.out.println(generateSalt("QuangPNCE170036", "group4prj301"));
+    }
 }
