50aec899fe7355b74ef9ff032a15930a559f9794
SolisCommet <nhatquangvl2003@gmail.com>
2022-10-11 12:18:35 +0700

    Analyze javadoc for some entities

--- src/main/java/entity/UserHealthInfo.java
+++ src/main/java/entity/UserHealthInfo.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package entity;
 
 /**
@@ -16,6 +12,15 @@ public class UserHealthInfo {
     private float activeness;
     private int age;
 
+    /**
+     *
+     * @param userId
+     * @param gender
+     * @param height
+     * @param weight
+     * @param activeness
+     * @param age
+     */
     public UserHealthInfo(int userId, String gender, float height, float weight, float activeness, int age) {
         this.userId = userId;
         this.gender = gender;
@@ -25,50 +30,98 @@ public class UserHealthInfo {
         this.age = age;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getUserId() {
         return userId;
     }
 
+    /**
+     *
+     * @param userId
+     */
     public void setUserId(int userId) {
         this.userId = userId;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getGender() {
         return gender;
     }
 
+    /**
+     *
+     * @param gender
+     */
     public void setGender(String gender) {
         this.gender = gender;
     }
 
+    /**
+     *
+     * @return
+     */
     public float getHeight() {
         return height;
     }
 
+    /**
+     *
+     * @param height
+     */
     public void setHeight(float height) {
         this.height = height;
     }
 
+    /**
+     *
+     * @return
+     */
     public float getWeight() {
         return weight;
     }
 
+    /**
+     *
+     * @param weight
+     */
     public void setWeight(float weight) {
         this.weight = weight;
     }
 
+    /**
+     *
+     * @return
+     */
     public float getActiveness() {
         return activeness;
     }
 
+    /**
+     *
+     * @param activeness
+     */
     public void setActiveness(float activeness) {
         this.activeness = activeness;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     *
+     * @param age
+     */
     public void setAge(int age) {
         this.age = age;
     }
