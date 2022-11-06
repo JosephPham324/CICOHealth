88f6bdfab1eccd7970d5fff7010d4152d3ef6026
SolisCommet <nhatquangvl2003@gmail.com>
2022-11-06 07:11:47 +0700

    HTML,CSS and JS for showing numbers

---
+++ src/main/webapp/scripts/todaynumbers.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let calEaten = document.getElementById("cal-eaten");
+    let allNumbers = document.querySelector(".all-numbers");
+    let content;
+
+    calEaten.addEventListener("click", () => {
+        if (allNumbers.classList.contains("active")) {
+            calEaten.textContent = content;
+            allNumbers.classList.remove("active");
+        } else {
+            content = calEaten.textContent;
+            calEaten.textContent = 'Hide';
+            allNumbers.classList.add("active");
+        }
+    });
+});
