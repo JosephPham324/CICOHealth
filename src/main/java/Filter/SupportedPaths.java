5759b2747071c305fdebddfe41f09e9f76ca71ac
SolisCommet <nhatquangvl2003@gmail.com>
2022-11-07 07:29:35 +0700

    Fix supported paths for admin

--- src/main/java/Filter/SupportedPaths.java
+++ src/main/java/Filter/SupportedPaths.java
@@ -13,6 +13,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -24,12 +26,12 @@ public class SupportedPaths {
     private HashMap<String, String> correspondingReferrer;//Key: servlet, value: referrer
 
     public SupportedPaths() {
-        UserDAO dao = new UserDAO();
-        ExerciseTypeDAO exDAO = new ExerciseTypeDAO();
-    	List<User> user = dao.getListUser();
-        List<User> admin = dao.getListAdmin();
-        List<ExerciseType> ex = exDAO.getAllExerciseTypes();
-        
+//        UserDAO dao = new UserDAO();
+//        ExerciseTypeDAO exDAO = new ExerciseTypeDAO();
+//        List<User> user = dao.getListUser();
+//        List<User> admin = dao.getListAdmin();
+//        List<ExerciseType> ex = exDAO.getAllExerciseTypes();
+
         correspondingReferrer = new HashMap<>();
         correspondingReferrer.put("/login-control", "/login");
         correspondingReferrer.put("/healthinfo-control", "/register-control");
@@ -47,35 +49,40 @@ public class SupportedPaths {
         correspondingReferrer.put("/edit-user-info-control", "/user-info");
         correspondingReferrer.put("/edit-health-info-control", "/user-info");
         correspondingReferrer.put("/edit-goal-control", "/user-info");
-        
-        for (User o : user) {
-            correspondingReferrer.put("/update-control?id="+o.getUserID(),"/login");
-            correspondingReferrer.put("/user-healthinfo?id="+o.getUserID(),"/login");
-            correspondingReferrer.put("/user-goal?id="+o.getUserID(),"/login");
-        }
-        for (User o : admin) {
-            correspondingReferrer.put("/update-control?id="+o.getUserID(),"/login");
-        }
-        for (ExerciseType exType : ex) {
-            correspondingReferrer.put("/update-exercisetype?exerciseid="+exType.getExerciseID(),"/login");
-        }
-        correspondingReferrer.put("/admin", "/login");
-        correspondingReferrer.put("/admin-control", "/login");
-        correspondingReferrer.put("/admin-control?action=ADMIN+INFO", "/login");
-        correspondingReferrer.put("/admin-control?action=USER+INFO", "/login");
-        correspondingReferrer.put("/admin-control?action=EXERCISE+MANAGEMENT", "/login");
-        
-        correspondingReferrer.put("/delete-user-control", "/login");
-        correspondingReferrer.put("/getuser-exerciseid-control", "/login");
-        correspondingReferrer.put("/user-load-control", "/login");
-        correspondingReferrer.put("/user-exercise", "/login");
-        
-        correspondingReferrer.put("/admin-exercisetype-control", "/login");
-        correspondingReferrer.put("/delete-exercisetype-control", "/login");
-        correspondingReferrer.put("/update-exercisetype-control", "/login");
-        correspondingReferrer.put("/add-exercisetype-control", "/login");
-        correspondingReferrer.put("/adminadd-exercise", "/login");
-        correspondingReferrer.put("/update-control","/login");
+
+//        for (User o : user) {
+//            correspondingReferrer.put("/update-control?id=" + o.getUserID(), "/login");
+//            correspondingReferrer.put("/user-healthinfo?id=" + o.getUserID(), "/login");
+//            correspondingReferrer.put("/user-goal?id=" + o.getUserID(), "/login");
+//        }
+//        for (User o : admin) {
+//            correspondingReferrer.put("/update-control?id=" + o.getUserID(), "/login");
+//        }
+//        for (ExerciseType exType : ex) {
+//            correspondingReferrer.put("/update-exercisetype?exerciseid=" + exType.getExerciseID(), "/login");
+//        }
+//        correspondingReferrer.put("/admin", "/login");
+//        correspondingReferrer.put("/admin-control?action=ADMIN+INFO", "/login");
+//        correspondingReferrer.put("/admin-control?action=USER+INFO", "/login");
+//        correspondingReferrer.put("/admin-control?action=EXERCISE+MANAGEMENT", "/login");
+
+        correspondingReferrer.put("/admin-control", "/admin");
+        correspondingReferrer.put("/admin", "/admin-admin-control");
+
+        correspondingReferrer.put("/update-healthinfo", "/admin-control");
+        correspondingReferrer.put("/user-goal", "/admin-control");
+
+        correspondingReferrer.put("/delete-user-control", "/admin-control");
+        correspondingReferrer.put("/getuser-exerciseid-control", "/admin-control");
+        correspondingReferrer.put("/user-load-control", "/admin-control");
+        correspondingReferrer.put("/user-exercise", "/admin-control");
+
+        correspondingReferrer.put("/admin-exercisetype-control", "/admin-control");
+        correspondingReferrer.put("/delete-exercisetype-control", "/admin-control");
+        correspondingReferrer.put("/update-exercisetype-control", "/admin-control");
+        correspondingReferrer.put("/add-exercisetype-control", "/admin-control");
+        correspondingReferrer.put("/adminadd-exercise", "/admin-control");
+        correspondingReferrer.put("/update-control", "/admin-control");
     }
 
     public boolean availableServlet(String servletPath) {
@@ -83,16 +90,26 @@ public class SupportedPaths {
     }
 
     public boolean checkCorrectReferrer(String servletPath, String referrerPath) {
+        String pattern = "(\\?[a-zA-z]+=.+)";
+        System.out.println(servletPath);
+        System.out.println(referrerPath);
+        servletPath = Pattern.compile(pattern).matcher(servletPath).replaceAll("");
+        referrerPath = Pattern.compile(pattern).matcher(referrerPath).replaceAll("");
+        System.out.println(servletPath);
+        System.out.println(referrerPath);
         if (referrerPath == null || servletPath == null || !this.correspondingReferrer.containsKey(servletPath)) {
+            System.out.println("lol");
+
             return false;
         }
         if (servletPath.endsWith("/home-control") || servletPath.endsWith("/logout-control") || servletPath.endsWith("/admin-control")) {
             return true;
         }
-        return referrerPath.endsWith("Nutrition" + this.correspondingReferrer.get(servletPath));
+
+        return referrerPath.endsWith(this.correspondingReferrer.get(servletPath));
     }
-    
-    public String getCorrectReferrer(String servletPath){
+
+    public String getCorrectReferrer(String servletPath) {
         return this.correspondingReferrer.get(servletPath);
     }
 
@@ -108,11 +125,9 @@ public class SupportedPaths {
 //        }
 //        return fileSet;
 //    }
-
 //    public String[] getUser_paths() {
 //        return user_paths;
 //    }
-
     public HashMap<String, String> getCorrespondingReferrer() {
         return correspondingReferrer;
     }
@@ -124,12 +139,8 @@ public class SupportedPaths {
 //            Logger.getLogger(SupportedPaths.class.getName()).log(Level.SEVERE, null, ex);
 //        }
 //    }
-    
     public static void main(String[] args) {
-        UserDAO user = new UserDAO();
-    	List<User> list = user.getListUser();
-        for (User o : list) {
-            System.out.println("/update-control?id="+o.getUserID());
-        }
+        SupportedPaths paths = new SupportedPaths();
+        System.out.println(paths.checkCorrectReferrer("/update-control?id=2", "/admin-control?action=ADMIN+INFO"));
     }
 }
