8daf6f74a930dcf9901f68abe4c23484193f1c76
SolisCommet <nhatquangvl2003@gmail.com>
2022-12-31 11:45:14 +0700

    Database context for Azure database connection

--- src/main/java/DAO/ExerciseDAO.java
+++ src/main/java/DAO/ExerciseDAO.java
@@ -11,6 +11,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -200,4 +202,12 @@ public class ExerciseDAO {
         ps.executeUpdate();
     }
 
+    public static void main(String[] args) {
+        ExerciseDAO dao = new ExerciseDAO();
+        try {
+            System.out.println(dao.getExerciseByUserID("1"));
+        } catch (SQLException ex) {
+            Logger.getLogger(ExerciseDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
--- src/main/java/context/DBContext.java
+++ src/main/java/context/DBContext.java
@@ -4,45 +4,56 @@ import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * Semester: FALL 2022
- * Subject : FRJ301
- * Class   : SE1606
- * Project : Nutrition 
- * @author : Group 4
- * CE161130  Nguyen Le Quang Thinh (Leader)
- * CE170036  Pham Nhat Quang
- * CE160464  Nguyen The Lu
- * CE161096  Nguyen Ngoc My Quyen
- * CE161025  Tran Thi Ngoc Hieu
+ * Semester: FALL 2022 Subject : FRJ301 Class : SE1606 Project : Nutrition
+ *
+ * @author : Group 4 CE161130 Nguyen Le Quang Thinh (Leader) CE170036 Pham Nhat
+ * Quang CE160464 Nguyen The Lu CE161096 Nguyen Ngoc My Quyen CE161025 Tran Thi
+ * Ngoc Hieu
  */
 public class DBContext {
 
-    private static final String dbPrefix = "jdbc:sqlserver://localhost:1433;databaseName=Nutrition [sa on SA]";
+    private static final String dbPrefix = "jdbc:sqlserver://quangpnce.database.windows.net:1433;database=script_nutrition;user=quangpnce170036@quangpnce;password=azure73768665!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
     private static final String dbPort = "1433";
-    private static final String databaseName = "Nutrition";
+    private static final String databaseName = "script_nutrition";
     private final String instance = "";
-    private static final String user = "sa";
-    private static final String pass = "123456";
+    private static final String user = "quangpnce170036@fpt.edu.vn";
+    private static final String pass = "azure73768665!";
+    private static final String connectionString = "jdbc:sqlserver://quangpnce.database.windows.net:1433;database=script_nutrition;user=quangpnce170036@quangpnce;password=azure73768665!;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30";
 
     /**
      * Connect to database
+     *
      * @return object Connection
      */
     public Connection getConnection() {
         Connection conn = null;
-        String dbURL = dbPrefix + ":" + dbPort + "\\" + instance + ";" + "databaseName=" + databaseName;
-        if (instance == null || instance.trim().isEmpty()) {
-            dbURL = dbPrefix + ":" + dbPort + ";" + "databaseName=" + databaseName;
-        }
+//        String dbURL = dbPrefix + ":" + dbPort + "\\" + instance + ";" + "databaseName=" + databaseName;
+//        if (instance == null || instance.trim().isEmpty()) {
+////            dbURL = dbPrefix + ":" + dbPort + ";" + "databaseName=" + databaseName;
+//              dbURL = connectionString;
+//        }
+//        try {
+//            DriverManager.registerDriver(new SQLServerDriver());
+//            conn = DriverManager.getConnection(dbURL, user, pass);
+//            System.out.println(dbPrefix);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+        String hostName = "quangpnce.database.windows.net";
+        String dbName = "script_nutrition";
+        String user = "quangpnce170036";
+        String password = "azure73768665!";
+        String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", hostName, dbName, user, password);
         try {
             DriverManager.registerDriver(new SQLServerDriver());
-            conn = DriverManager.getConnection(dbURL, user, pass);
-        } catch (SQLException e) {
-            e.printStackTrace();
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
         return conn;
     }
 }
-
